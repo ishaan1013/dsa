59e6acd45b6e50ebb25c7d36f0cc3748eed8dc7e
ishaan1013 <ishaan1013@gmail.com>
2022-12-26 13:43:04 -0500

    217 java

---
+++ lc-java/217.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> uniques = new HashSet<>();
+    for (int i = 0; i < nums.length; i++) {
+      if (uniques.contains(nums[i])) {
+        return true;
+      }
+      uniques.add(nums[i]);
+    }
+    return false;
+  }
+}
