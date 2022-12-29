7684d9c16ee5809b4fe9a0c0a35f6ad48672daf8
ishaan1013 <ishaan1013@gmail.com>
2022-12-29 11:49:40 -0500

    100

---
+++ lc-java/100.java
@@ -0,0 +1,16 @@
+class Solution {
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) {
+      return true;
+    }
+    if (p == null || q == null) {
+      return false;
+    }
+    if (p.val == q.val) {
+      boolean l = isSameTree(p.left, q.left);
+      boolean r = isSameTree(p.right, q.right);
+      return l && r;
+    }
+    return false;
+  }
+}
